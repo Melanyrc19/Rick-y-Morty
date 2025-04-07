4846571fa07b523fb3026322b7d6ddaca986716e
MelanyRodriguez <melanyrc.19@gmail.com>
2025-04-06 21:48:26 -0300

    modificacion de estilos

--- index.js
+++ index.js
@@ -55,16 +55,28 @@ function pintarDatos(array) {
     if(esLaVistaDePersonajes) {
 
     for (const personaje of array) {
-        $containerImages.innerHTML += `<div class="p-2 m-auto "><img class="w-24 h-24 md:w-48 md:h-48 object-cover rounded-lg mb-4" src="${personaje.image}" />
-        <h2>${personaje.name}</h2>
-        <p>Genero: ${personaje.gender}</p>
-        <p>Estado: ${personaje.status}</p>`;
+        $containerImages.innerHTML += ` <div class="sm:w-4/6 md:w-60 p-6 flex flex-col justify-start items-center m-4 rounded-lg bg-gradient-to-br from-green-400 to-blue-500 shadow-lg md:transform md:transition md:duration-300 md:ease-in-out md:hover:scale-110 md:hover:rotate-3">
+        <img class="w-auto h-24 md:w-56 md:h-56 md:object-cover rounded-lg mb-4 border-4 border-yellow-500 md:transition md:duration-300 md:ease-in-out md:transform md:hover:scale-110" src="${personaje.image}" />
+        <h2 class="md:text-2xl font-bold text-white mb-2 font-[Comic Sans MS], sans-serif">${personaje.name}</h2>
+        <p class="text-white mb-1"><strong>Género:</strong> ${personaje.gender}</p>
+        <p class="text-white mb-4"><strong>Estado:</strong> ${personaje.status}</p>
+        <button class="bg-yellow-400 text-black py-2 px-6 rounded-full shadow-lg transition duration-300 ease-in-out hover:scale-110 hover:bg-yellow-500 focus:outline-none">
+            Ver Más
+        </button>
+    </div>
+    `;
     }}
     else{
         for (const capitulo of array) {
             $containerImages.innerHTML += `
-            <h2>${capitulo.name}</h2>
-            <p>Episodio: ${capitulo.episode}</p>`;
+         <div class="sm:w-4/6 sm:w-60 p-6 flex flex-col justify-start items-center m-6 my-12 rounded-lg shadow-2xl bg-gradient-to-br from-green-500 to-blue-600 h-60 md:transform md:transition duration-300 md:ease-in-out md:hover:scale-110 md:hover:rotate-3">
+            <h2 class="md:text-2xl font-extrabold text-white mb-3 text-center font-[Comic Sans MS], sans-serif">${capitulo.name}</h2>
+            <p class="text-lg text-white mb-3 text-center">Episodio: ${capitulo.episode}</p>
+            <button class="bg-yellow-400 text-black py-2 px-6 rounded-full shadow-lg transform transition duration-300 ease-in-out hover:scale-110 hover:bg-yellow-500 focus:outline-none">
+                Ver Más
+            </button>
+        </div>
+        `;
     }
 }}
 
@@ -85,8 +97,8 @@ function pintarCapitulos(arrayCapitulos) {
         $containerImages.innerHTML = "<p>No se encontraron capitulos.</p>";
     } else {
     for (const capitulo of arrayCapitulos) {
-        $containerImages.innerHTML += `<div class="p-2 m-auto ">
-        <h2>${capitulo.name}</h2>
+        $containerImages.innerHTML += `<div class="b-2 flex w-24 h-12 p-2 m-auto ">
+        <h2 class="text-3xl">${capitulo.name}</h2>
         <p>Genero: ${capitulo.gender}</p>
         <p>Estado: ${capitulo.status}</p>`;
     }}
